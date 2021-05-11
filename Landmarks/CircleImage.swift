b55558e8bf3fc415dba7f7dbc5ba29bf796f6f17
FeiqiongDeng <feiqiongd@kingsbridgebcp.com>
2021-05-11 15:25:51 -0400

    create an image view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//   CircleImage.swift
+//  Landmarks
+//
+//  Created by 邓飞琼 on 2021-05-11.
+//
+
+import SwiftUI
+
+struct _CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        _CircleImage()
+    }
+}
