0937a29ffe518db9838fbac591cef971b61bd9e8
FeiqiongDeng <feiqiongd@kingsbridgebcp.com>
2021-05-11 13:34:11 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 邓飞琼 on 2021-05-11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
